02d7ed644c538a0683177ed528d801c92d0a7599
simonfly80 <simonfly80@gmail.com>
2016-06-04 09:24:17 +0200

    Update cinemalibero.py
    Corretto il nome del plugin, rimosso master.

--- channels/cinemalibero.py
+++ channels/cinemalibero.py
@@ -183,7 +183,7 @@ def peliculas(item):
 
 def HomePage(item):
     import xbmc
-    xbmc.executebuiltin("ReplaceWindow(10024,plugin://plugin.video.streamondemand-pureita-master)")
+    xbmc.executebuiltin("ReplaceWindow(10024,plugin://plugin.video.streamondemand-pureita)")
 
 def info(title):
     logger.info("streamondemand.cinemalibero info")
