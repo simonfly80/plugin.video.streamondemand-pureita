c8bf66e2a08ad039fd13e9100e133a6110ac1729
simonfly80 <simonfly80@gmail.com>
2016-06-04 09:31:02 +0200

    Update filmstreampw.py

--- channels/filmstreampw.py
+++ channels/filmstreampw.py
@@ -252,7 +252,7 @@ def peliculas(item):
 
 def HomePage(item):
     import xbmc
-    xbmc.executebuiltin("ReplaceWindow(10024,plugin://plugin.video.streamondemand-pureita-master)")
+    xbmc.executebuiltin("ReplaceWindow(10024,plugin://plugin.video.streamondemand-pureita)")
 
 def episodios(item):
     logger.info("streamondemand.channels.filmstreampw episodios")
